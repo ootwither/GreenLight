ad9ebb2545e667429fc04ec5d0cf64d52c28a24f
ootwither <dougal.wallace@gmail.com>
2024-01-11 10:38:55 +0000

    feat: adds default item data, renders items

---
+++ client/src/assets/items.js
@@ -0,0 +1,29 @@
+const items = [
+  {
+    task: 'drink water',
+    short: 'D W',
+    id: 1
+  },
+  {
+    task: 'take brain drugs',
+    short: 'TBD',
+    id: 2
+  },
+  {
+    task: 'stretch',
+    short: 'STR',
+    id: 3
+  },
+  {
+    task: 'Tell Luca you love her',
+    short: 'LL',
+    id: 4
+  },
+  {
+    task: 'feed yozi',
+    short: 'FY',
+    id: 5
+  },
+];
+
+export default items;
