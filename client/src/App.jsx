f5451f7a3006e90ed9be6629c4ad9147560c761c
ootwither <dougal.wallace@gmail.com>
2024-01-12 14:22:47 +0000

    feat: buttons display warning at appropriate interval

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,64 @@
 /* eslint-disable react/prop-types */
-// import { useState } from 'react'
 import items from './assets/items'
-
-
 import './App.css'
+import { useEffect, useState } from 'react'
 
-function ButtonGrid({items}) {
+function ButtonGrid({time}) {
+  const [tasks, setTasks] = useState(items);
   return (
     <>
-    {items.map(item =>
-    <button key={item.id} onClick={item => item.toggle = !item.toggle}>
-      <div className={item.toggle ? 'selected' : ''}>
-      {item.short}
-      </div>
-      <div>OFF</div>
-      </button>
-        )}
-    </>
-  )
-}
-
-function ConfigurePanel() {
-  return (
-    <>
-    <form>
-
-    </form>
-
+      {tasks.map(item =>
+        <button key={item.id}
+        onClick={() => {
+          setTasks(
+            tasks.map((task) => {
+              return task.id === item.id ? {
+                ...task, lastChecked : Date.now()
+              } : task;
+            })
+          )
+        }}
+        >
+          <div className={time > (item.lastChecked + item.interval) ? 'WARNING' : ''}>
+            {item.short}
+          </div>
+        </button>
+      )}
     </>
   )
 }
 
 function App() {
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(Date.now());
+    }, 1000)
+  })
 
   return (
-    <>
-    <ButtonGrid items={items}/>
+    <><div>{time}</div>
+      <ButtonGrid time={time} />
     </>
   )
 }
 
 export default App
+
+
+// function ConfigurePanel() {
+//   return (
+//     <>
+//     <form>
+
+//     </form>
+
+//     </>
+//   )
+// }
+
+        // className={item.toggle ? 'selected' : ''}
+
+
+
