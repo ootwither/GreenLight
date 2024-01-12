b97871099f61b824154e7bedd2fe3828e322a39e
ootwither <dougal.wallace@gmail.com>
2024-01-12 14:58:52 +0000

    feat: modifies css, adds custom palette to root

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,27 +3,29 @@ import items from './assets/items'
 import './App.css'
 import { useEffect, useState } from 'react'
 
-function ButtonGrid({time}) {
+function ButtonGrid({ time }) {
   const [tasks, setTasks] = useState(items);
   return (
     <>
-      {tasks.map(item =>
-        <button key={item.id}
-        onClick={() => {
-          setTasks(
-            tasks.map((task) => {
-              return task.id === item.id ? {
-                ...task, lastChecked : Date.now()
-              } : task;
-            })
-          )
-        }}
-        >
-          <div className={time > (item.lastChecked + item.interval) ? 'WARNING' : ''}>
-            {item.short}
-          </div>
-        </button>
-      )}
+      <div className='buttonGrid'>
+        {tasks.map(item =>
+          <button key={item.id}
+            onClick={() => {
+              setTasks(
+                tasks.map((task) => {
+                  return task.id === item.id ? {
+                    ...task, lastChecked: Date.now()
+                  } : task;
+                })
+              )
+            }}
+          >
+            <div className={time > (item.lastChecked + item.interval) ? 'WARNING' : ''}>
+              {item.short}
+            </div>
+          </button>
+        )}
+      </div>
     </>
   )
 }
@@ -35,11 +37,14 @@ function App() {
     setInterval(() => {
       setTime(Date.now());
     }, 1000)
-  })
+  }, [])
 
   return (
-    <><div>{time}</div>
+    <>
+      {/* <div>{time}</div> */}
+
       <ButtonGrid time={time} />
+
     </>
   )
 }
@@ -58,7 +63,7 @@ export default App
 //   )
 // }
 
-        // className={item.toggle ? 'selected' : ''}
+// className={item.toggle ? 'selected' : ''}
 
 
 
